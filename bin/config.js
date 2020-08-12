b56174e85ef4f9a881524264712fa5e30baf64c2
e.mordred <evertonmantena@gmail.com>
2020-08-11 23:05:20 -0300

    first commit

--- bin/config.js
+++ bin/config.js
@@ -4,22 +4,22 @@ const config = () => {
     switch (env) {
         case 'dev':
         return {
-            bd_string: '',
-            jwt_pass: '',
-            jwt_expires_in: ''
+            bd_string: 'mongodb+srv://node-api:6uHNLA6kAa7wwepx@nodeapidemo-c3ob6.mongodb.net/NodeApi?retryWrites=true&w=majority',
+            jwt_pass: 'jfasdofjiof342342kjki4$@#$@#dsakdfsaf',
+            jwt_expires_in: '7d'
         }
         case 'hml':
         return {
-            bd_string: '',
-            jwt_pass: '',
-            jwt_expires_in: ''
+            bd_string: 'mongodb+srv://node-api:6uHNLA6kAa7wwepx@nodeapidemo-c3ob6.mongodb.net/NodeApi?retryWrites=true&w=majority',
+            jwt_pass: 'jfasdofjiof342342kjki4$@#$@#dsakdfsaf',
+            jwt_expires_in: '7d'
         }
 
         case 'prod':
         return {
-            bd_string: '',
-            jwt_pass: '',
-            jwt_expires_in: ''
+            bd_string: 'mongodb+srv://node-api:6uHNLA6kAa7wwepx@nodeapidemo-c3ob6.mongodb.net/NodeApi?retryWrites=true&w=majority',
+            jwt_pass: 'jfasdofjiof342342kjki4$@#$@#dsakdfsaf',
+            jwt_expires_in: '7d'
         }
     }
 }
